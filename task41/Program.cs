c2cad761c04d0b62d1766594936dac08e5fe78a5
Egor Еtrofimov <martynenkoa01@gmail.com>
2022-08-26 22:39:16 +0300

    final

--- task41/Program.cs
+++ task41/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите числа = ");
+int[] array = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
+int a = 0;
+
+for (int j = 0; j < array.Length; j++)
+{
+    if (array[j] > 0)
+    {
+        a = a+1;
+    }
+}
+Console.WriteLine(" ");
+Console.WriteLine(string.Join(",", a));
+
